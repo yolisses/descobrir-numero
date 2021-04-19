d9a8d53523c5125f42656cecdec3abb5ad33c296
Yowlisses <m.stunik@gmail.com>
2021-04-18 21:29:54 -0300

    Adiciona mensagens do status

--- script.js
+++ script.js
@@ -1,42 +1,31 @@
-
 let contador = 0
-let numeroGerado = Math.floor(Math.random() * 100); 
+let numeroGerado = Math.floor(Math.random() * 100);
 console.log(numeroGerado)
 
-document.getElementById("form").onsubmit=(event)=>{
+document.getElementById("form").onsubmit = (event) => {
     event.preventDefault()
     testar()
-    
-    console.log("teste")
-
 }
 
-function testar (){
+const status = document.querySelector(".status")
+status.innerHTML = "<spam style='color:red'>Digite algum valor</spam>"
+
+function testar() {
     let tentativas = document.getElementById("tentativa")
     console.log("entrou na função")
     let numeroDigitado = document.getElementById("chute").value
     console.log(numeroDigitado)
-    contador+=1
-    document.getElementById("tentativa").innerHTML= `tentativa: ${contador} `
+    contador += 1
+    document.getElementById("tentativa").innerHTML = `Tentativa${contador > 1 ? 's' : ''}: <spam style="color:blue">${contador}</spam> `
 
-    
-    if (numeroGerado == numeroDigitado){
-        console.log("igualzin doid")
-        
+
+    if (numeroGerado == numeroDigitado) {
+        status.innerHTML = "<spam style='color:green'>Parabéns, você acertou!!</spam>"
     }
-    else if (numeroGerado > numeroDigitado){
-        console.log("numero é maior")
-        console.log(contador)        
-        
-
-
+    else if (numeroGerado > numeroDigitado) {
+        status.innerHTML = "O número sorteado é maior"
     }
-    else if (numeroGerado < numeroDigitado){
-        console.log("numero é menor")
-        console.log(contador)
-        
-        
-
-
+    else if (numeroGerado < numeroDigitado) {
+        status.innerHTML = "O número sorteado é menor"
     }
 }
